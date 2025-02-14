8bc1ae1ab9bd12f04242cfc8f0550354b858ba1c
lukathompson <ldt05sf@gmail.com>
2025-02-14 11:38:22 +0100

    Initial commit

---
+++ Question_1.py
@@ -0,0 +1,27 @@
+print(type(2+3))
+
+print(type(6/2))
+
+print(type(6//2))
+
+print(type(2 != 3))
+
+print(type(5 or 6))
+
+print(type(print))
+
+print(type(print(2*2)))
+
+print(type("abc".find))
+
+print([] or "" or 0/10 or 7/2 or 9)
+
+print(9 and 10 and 7/2 and "" and 11/2)
+
+print(len(["abc", 2]))
+
+a = 2
+b = 3
+c = "abc"
+print(a,b,c)
+
