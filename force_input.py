1c07fa63e98aa4e5b794a1e5db41bd5280703718
lukathompson <ldt05sf@gmail.com>
2025-02-16 20:03:38 +0100

    more additions

---
+++ force_input.py
@@ -0,0 +1,18 @@
+def multiple_of_six():
+    """
+    Returns a mulitple of 6, keeps asking otherwise
+    :return: int
+    """
+    while True:
+        try:
+            n =input("Please give me a multiple of 6: ")
+            n = int(n)
+
+            if n % 6 == 0:
+                return n
+            else:
+                print("that is not a multiple of 6!")
+        except ValueError:
+            print("that is not a valid number")
+
+multiple_of_six()
